635eafe5260537b6ffbe29f07fc9d9923e1bab60
Chris Cardle <cdcardle@gmail.com>
2018-10-30 15:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function retrieveEmployeeInformation() {
 
 function addNewElementAsLi() {
   let input = retrieveEmployeeInformation();
-  document.querySelector('ul').prepend('<li>input</li>');
+  $('ul').prepend('<li>input</li>');
 }
 
 function addNewLiOnClick() {
